50d65f6ab7975d5f9824903631c2c880ebbc8dbc
George Belsten <George.Belsten@metaswitch.com>
2017-06-07 15:52:57 +0100

    Add MIB verification to cw_mib_generator.py

--- mib-generator/cw_mib_generator.py
+++ mib-generator/cw_mib_generator.py
@@ -17,6 +17,7 @@ Options:
 
 from __future__ import print_function
 import sys
+import subprocess
 import os.path
 from string import Template
 from docopt import docopt
@@ -70,7 +71,7 @@ def generate_mib(common_dir, extras_dir, common_mib, extras_mib, output_mib):
     try:
         common_src = common_src_template.substitute(substitute_dict)
     except ValueError:
-        print("ERROR - Common MIB fragment contains an invalid placeholder!")
+        print("ERROR: Common MIB fragment contains an invalid placeholder!")
         raise
     except KeyError:
         print("ERROR: Common MIB fragment contains unrecognised placeholder!")
@@ -80,6 +81,9 @@ def generate_mib(common_dir, extras_dir, common_mib, extras_mib, output_mib):
     extras_src = read_mib_fragment(extras_mib_path)
     full_mib_data = common_src + extras_src
     write_mib_file(output_mib_path, full_mib_data)
+
+    # Verify that the MIB is valid
+    verify_mib(common_dir, output_mib_path)
 
 
 def generate_title(mib_name):
@@ -108,6 +112,21 @@ def write_mib_file(mib_path, mib_contents):
         print("ERROR: Could not write MIB file!")
         raise
 
+
+def verify_mib(common_dir, mib_path):
+    smilint_conf = os.path.join(common_dir, "smilint.conf")
+    try:
+        cmd = ['smilint', '-s', '-c', smilint_conf, mib_path]
+        p = subprocess.Popen(cmd, stderr=subprocess.PIPE)
+        _, stderr = p.communicate()
+
+    except:
+        print("ERROR: Could not run smilint!")
+        raise
+
+    if p.returncode != 0 or len(stderr) != 0:
+        print(stderr)
+        sys.exit("ERROR: MIB failed validation!")
 
 if __name__ == "__main__":
     args = docopt(__doc__)
